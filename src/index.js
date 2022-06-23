cbad535e257bfb2890211adb58957bc899a19f74
Maria <maria.caram@gmail.com>
2022-06-23 20:18:50 -0300

    Se agrega nodemon

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import express from 'express'
+
+
+const app = express();
+
+const port = 4000
+
+app.listen(port, ()=>{
+    console.log ("estoy usando el puerto" + port)
+})
+
 console.log ("hola mundo")
