86899b9379f0610248611c1091a75e751cb5723f
Maria <maria.caram@gmail.com>
2022-06-23 21:13:22 -0300

    se agrega el sistema de rutas y midlewares

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import express from 'express'
-
+import morgan from "morgan"
+import cors from "cors"
 
 const app = express();
 
-const port = 4000
+app.set ("port", process.env.PORT || 4000)
 
-app.listen(port, ()=>{
-    console.log ("estoy usando el puerto" + port)
+app.listen(app.get("port"), ()=>{
+    console.log ("estoy usando el puerto" + app.get ("port"))
 })
 
 console.log ("hola mundo")
+
+//configuraciones de midlewares
+
+app.use(morgan("dev"));
+app.use(cors())
+app.use(express.json())
+express.urlencoded ({extended:true})
+
+//crear ruta
+
+app.get("/", (req,res) => {res.send ("esto es una repsuesta desde el backend")})
+app.delete("/borrarproducto", (req,res) => {res.send ("peticion para borrar")})
